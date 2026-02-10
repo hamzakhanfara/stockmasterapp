c0aff64ffa32c0d7633dcd6bb08a1b04c9104717
Hamza KHANFARA <khanfarahamza@gmail.com>
2026-02-10 16:44:34 +0100

    Simplify Prisma client initialization

--- backend/src/prisma/index.ts
+++ backend/src/prisma/index.ts
@@ -1,10 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
 
-export const prisma = new PrismaClient({
-  adapter: new PrismaPg({
-    connectionString: process.env.DATABASE_URL!,
-  }),
-});
+export const prisma = new PrismaClient();
 
 export * from "@prisma/client";
